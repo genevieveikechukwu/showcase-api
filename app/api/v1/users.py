9de57f76becbd11e250d19a49bceff1c48bb1bf2
Genevieve Ikechukwu <ikechukwummachukwu2020@gmail.com>
2023-04-04 17:32:33 +0100

    changes

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI, Depends, APIRouter
 from sqlalchemy.orm import Session
 from app.database.database import get_db
-from fastapi_pagination import add_pagination
 
 
 router = APIRouter()
-add_pagination(router)
+
 
 @router.get("/users")
 def users():
